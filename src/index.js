705d9f086c6360a7b0843032a6b66138730e8616
Cody Nguyen <cody@simicart.com>
2021-04-08 16:09:31 +0700

    Add network1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.css'
+import { sendRequest } from './Network/GraphQl'
+
+const GET_ITEM_QUERY = `
+    query getPbItem($pageMaskedId: String) {
+        spb_item(pageMaskedId: $pageMaskedId) {
+            total_count
+            items {
+                entity_id
+                name
+            }
+        }
+    }
+`;
 
 export const PageBuilderComponent = ({ endPoint, maskedId }) => {
+    const [data, setData] = useState(false);
+    if (!data) {
+        console.log('a')
+        sendRequest(
+            endPoint,
+            data => {
+                console.log(data);
+                setData(data);
+            },
+            GET_ITEM_QUERY,
+            { pageMaskedId: maskedId },
+            "getPbItem"
+        )
+    }
     return <div className={styles.test}>PageBuilder Component for: endPoint: {endPoint}, maskedId: {maskedId}</div>
 }
