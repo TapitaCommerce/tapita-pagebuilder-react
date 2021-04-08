5cc01138507fc5151784bd8d03ecdad03af6f5e5
Cody Nguyen <cody@simicart.com>
2021-04-08 17:29:13 +0700

    Update inner content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import styles from './styles.module.css'
 import { sendRequest } from './Network/GraphQl'
+import Content from './Content'
 
 const GET_ITEM_QUERY = `
     query getPbItem($pageMaskedId: String) {
-        spb_item(pageMaskedId: $pageMaskedId) {
+        spb_page(pageMaskedId: $pageMaskedId) {
             total_count
             items {
                 entity_id
                 name
+                masked_id
+                custom_css
+            }
+        }
+        spb_item(pageMaskedId: $pageMaskedId) {
+            total_count
+            items {
+                entity_id
+                page_id
+                parent_id
+                styles
+                data
+                name
+                class_name
+                type
+                visibility
+                storeview_visibility
+                sort_order
             }
         }
     }
@@ -17,17 +35,18 @@ const GET_ITEM_QUERY = `
 export const PageBuilderComponent = ({ endPoint, maskedId }) => {
     const [data, setData] = useState(false);
     if (!data) {
-        console.log('a')
         sendRequest(
             endPoint,
-            data => {
-                console.log(data);
-                setData(data);
+            result => {
+                setData(result);
             },
             GET_ITEM_QUERY,
             { pageMaskedId: maskedId },
             "getPbItem"
         )
     }
-    return <div className={styles.test}>PageBuilder Component for: endPoint: {endPoint}, maskedId: {maskedId}</div>
+    if (data && data.data && data.data.spb_item && data.data.spb_item.items && data.data.spb_page) {
+        return <Content data={data.data} />
+    }
+    return '';
 }
