ead77b07df78fa9c9d036019933ed3b64a18955d
Cody Nguyen <cody@simicart.com>
2021-11-23 09:35:21 +0700

    Update example

---
+++ example/src/AppWithRouter.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import { PageBuilderComponent, usePbFinder } from 'simi-pagebuilder-react';
+import { useIntl } from 'react-intl';
+import {
+	Route,
+	Switch,
+	useLocation,
+	BrowserRouter,
+	Link,
+	useHistory,
+} from 'react-router-dom';
+
+const endPoint = 'https://tapita.io/pb/graphql';
+const integrationToken = '105iuL0eUSr6vmjVE5fNp6zPWSuE47hqh1637550214';
+
+const AppWithRouter = () => {
+	const location = useLocation();
+	const history = useHistory();
+	const pbFinderProps = usePbFinder({
+		endPoint,
+		integrationToken,
+	});
+	const { loading: pbLoading, findPage, pathToFind } = pbFinderProps;
+	let { pageMaskedId, pageData } = pbFinderProps;
+	useEffect(() => {
+		if (location && location.pathname) {
+			if (!pageMaskedId || location.pathname !== pathToFind)
+				findPage(location.pathname);
+		}
+	}, [location, pageMaskedId, pathToFind, findPage]);
+	if (pageMaskedId && pageMaskedId !== 'notfound') {
+		const pbcProps = {
+			Link: Link,
+			history: history,
+			endPoint,
+			lazyloadPlaceHolder: <div />,
+		};
+		return (
+			<PageBuilderComponent
+				{...pbcProps}
+				key={pageMaskedId}
+				maskedId={pageMaskedId}
+				pageData={pageData && pageData.publish_items ? pageData : false}
+			/>
+		);
+	}
+	return <div>loading ...</div>;
+};
+const HOAppWithRouter = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route render={(props) => <AppWithRouter {...props} />} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
+export default HOAppWithRouter;
