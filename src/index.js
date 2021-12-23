6969d89472546662c7a95a578ca8d3a9b359c094
Cody Nguyen <cody@simicart.com>
2021-12-23 11:56:32 +0700

    Umd 1.3.8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 //uncomment this to build standalone package
-//import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import { sendRequest } from './Network/GraphQl';
 import Content from './Content';
 import { Helmet } from 'react-helmet';
@@ -221,6 +221,7 @@ export const PageBuilderComponent = (props) => {
 };
 
 // comment this to build standalone package
+/*
 export const usePbFinder = (props) => {
 	const { endPoint, integrationToken, storeCode } = props;
 	const [pbData, setPbData] = useState(false);
@@ -294,9 +295,8 @@ export const usePbFinder = (props) => {
 		allPages: pbData,
 	};
 };
-/*
-uncomment this to build standalone package
+*/
+
 export const renderForIdWithProps = (elId, pbProps) => {
 	ReactDOM.render(<PageBuilderComponent {...pbProps} />, document.getElementById(elId));
 }
-*/
