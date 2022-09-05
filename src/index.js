e220b1309731cdefe8fe41257d92e715eec37870
alitonia <allyofjustice1@gmail.com>
2022-09-05 13:43:02 +0700

    Add type definition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // uncomment this to build standalone package
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { sendRequest } from './Network/GraphQl';
 import Content from './Content';
 import { Helmet } from 'react-helmet';
@@ -275,9 +275,6 @@ export const usePbFinder = (props) => {
 				const extraHint = (function (basePath) {
 					try {
 						if (minimized) {
-							// if (basePath === '' || basePath === '/') {
-							// 	return '/'
-							// }
 							if (args.hint) {
 								return args.hint;
 							}
@@ -367,9 +364,9 @@ export const usePbFinder = (props) => {
 
 export { TreeDataProductDetailMarkerEnum } from './Helper/treeDataUtils';
 // uncomment this to build standalone package
-// export const renderForIdWithProps = (elId, pbProps) => {
-// 	ReactDOM.render(
-// 		<PageBuilderComponent {...pbProps} />,
-// 		document.getElementById(elId),
-// 	);
-// };
+export const renderForIdWithProps = (elId, pbProps) => {
+	ReactDOM.render(
+		<PageBuilderComponent {...pbProps} />,
+		document.getElementById(elId),
+	);
+};
