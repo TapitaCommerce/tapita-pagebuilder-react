5e0daf361ce18c6e5719784693ee5a82be226c13
codynguyen <>
2022-12-01 11:06:51 +0700

    Add offset with higher value

--- src/Content/index.js
+++ src/Content/index.js
@@ -281,7 +281,7 @@ const PbContent = (props) => {
 			!item.dataParsed.scrollTo
 		) {
 			return (
-				<LazyLoad {...itemProps} placeholder={lazyloadPlaceHolder} offset={100}>
+				<LazyLoad {...itemProps} placeholder={lazyloadPlaceHolder} offset={532}>
 					{innerContent}
 				</LazyLoad>
 			);
